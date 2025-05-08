44ebe1e7706bee82d41011c38bfeb754b74dbe94
sahil <sahil.malik@thewasserstoff.com>
2025-05-08 12:37:34 +0530

    remove build err

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 import { Loader2 } from "lucide-react";
 
-export default function () {
+export default function Loading () {
   return (
     <div className="text-foreground max-h-[100dvh] h-[100dvh] portrait:max-h-[100dvh] portrait:h-[100dvh] w-full flex items-center justify-center bg-background prose-headings:font-heading prose-h1:md:text-5xl prose-h1:text-3xl">
       <Loader2 className="animate-spin text-main" size={50} />
